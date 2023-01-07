14d400152e55a4d767598e8c250b86a4861d35bd
parsa jiravand <parsajiravand@gmail.com>
2023-01-08 00:23:21 +0330

    create category entity

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
+
 // import routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
 
 // app
 const app = express();
@@ -24,6 +26,7 @@ app.use(expressValidator());
 // routes middleware
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
 
 const port = process.env.PORT || 8000;
 
