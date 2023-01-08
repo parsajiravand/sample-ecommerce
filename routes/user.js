9443269247021af8b19ebed13689fb49ba5612c6
parsa jiravand <parsajiravand@gmail.com>
2023-01-09 03:23:28 +0330

    add READ and UPDATE api for user

--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,16 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 
-const { userById } = require("../controllers/user");
+const { userById, read, update } = require("../controllers/user");
 
 router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
   res.json({
     user: req.profile,
   });
 });
+
+router.get("/user/:userId", read);
+router.put("/user/:userId", update);
 
 router.param("userId", userById);
 
